0fd888a9dcc8ff2c8612036ea54009b13a3b528c
YourGitHubUsername <your.email@example.com>
2025-11-15 12:52:11 +0100

    feat: Teacher interface with mandatory fullTimeEmployee and location

--- Teacher.ts
+++ Teacher.ts
@@ -6,3 +6,23 @@ interface Teacher {
   location: string;             
 
 }
+
+const Teacher: Teacher = {
+  firstName: "Ada",           
+  lastName: "Loveth",      
+  fullTimeEmployee: true,     
+  location: "Lagos",         
+  contract: false          
+};
+
+const goodTeacher: Teacher = {
+  firstName: "Mohammed",
+  lastName: "Mike",
+  fullTimeEmployee: false,    
+  location: "Lagos",         
+  yearsOfExperience: 5,       
+  department: "English"          
+};
+
+const TeacherList:Teacher[]= [teacher];
+
--- js/main.ts
+++ js/main.ts
@@ -17,4 +17,4 @@ const teacher: Teacher = {
   contract: false,
 };
 
-const TeacherList:Teacher[]= [teacher];
+const TeachersList:Teacher[]= [teacher];
